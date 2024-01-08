1354ead31cb3cf5cc4c6661e68640a9371e6bbed
TRegan5 <tpregan5@gmail.com>
2024-01-07 21:19:21 -0500

    Completed #1

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -1,11 +1,14 @@
 -- Active: 1704240860845@@127.0.0.1@5432@employee
 # 1. List the employee number, last name, first name, sex, and salary of each employee
-select emp_no, last_name, first_name, sex from 
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s
+ON e.emp_no = s.emp_no;
 
 # 2. List the first name, last name, and hire date for the employees who were hired in 1986
-select first_name, last_name, hire_date
-from employees
-where hire_date LIKE '%1986';
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
 
 # 3. List the manager of each department along with their department number, department name, employee number, last name, and first name
 
@@ -14,9 +17,9 @@ where hire_date LIKE '%1986';
 
 
 # 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
-select first_name, last_name, sex
-from employees
-where first_name = 'Hercules' and last_name LIKE 'B%';
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 # 6. List each employee in the Sales department, including their employee number, last name, and first name
 
