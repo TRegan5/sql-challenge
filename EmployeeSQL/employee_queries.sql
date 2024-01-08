7cf49af503bfce98ec1b99dabf3e13910ea39980
TRegan5 <tpregan5@gmail.com>
2024-01-07 21:13:27 -0500

    Completed #5 and added image of ERD

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -1,4 +1,4 @@
--- Active: 1704240860845@@127.0.0.1@5432@employee@public
+-- Active: 1704240860845@@127.0.0.1@5432@employee
 # 1. List the employee number, last name, first name, sex, and salary of each employee
 select emp_no, last_name, first_name, sex from 
 
@@ -14,7 +14,9 @@ where hire_date LIKE '%1986';
 
 
 # 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
-
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name LIKE 'B%';
 
 # 6. List each employee in the Sales department, including their employee number, last name, and first name
 
